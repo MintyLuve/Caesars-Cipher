f6740a014ffe397e69dadd7370ac765f8789f0ca
Sophia <111466302+MintyLuve@users.noreply.github.com>
2024-08-02 05:33:03 +0000

    Cracking Caesar's cipher

--- main.py
+++ main.py
@@ -14,14 +14,6 @@ initPos = 0
 shiftPos = 0
 shiftMsg = ""
 
-# Define the function called encryptOrDecrypt
-def encOrDec():
-  global shiftPos
-  if mode == "decrypt":
-    shiftPos = initPos - int(key)
-  elif mode == "encrypt":
-    shiftPos = initPos + int(key)
-
 # Define the function called wraparound
 def wrap():
   global shiftPos
@@ -37,19 +29,39 @@ print("Welcome! This program will encrypt or decrypt your secret message using t
 
 # Receive user input
 initMsg = input("What is your message?\n")
-key = input("What is the key? Choose a number from 0 to "+str(len(possChar)-1)+"\n")
 mode = input("Do you want to decrypt or encrypt?\n")
 
-# Encrypt or decrypt the message
+if mode == "encrypt":
+  key = input("What is the key? Choose a number from 0 to "+str(len(possChar)-1)+"\n")
 
-for character in initMsg:
-  if character in possChar:
-    initPos = possChar.find(character)
-    encOrDec()
-    wrap()
-    shiftMsg += possChar[shiftPos]
-  else:
-    shiftMsg+= character
+  for character in initMsg:
+    if character in possChar:
+      initPos = possChar.find(character)
+      shiftPos = initPos + int(key)
+      wrap()
+      shiftMsg += possChar[shiftPos]
+    else:
+      shiftMsg+= character
 
-# Print the shifted message
-print("Your message is\n"+shiftMsg)
+  # Print the shifted message
+  print("Your message is\n"+shiftMsg)
+
+elif mode=="decrypt":
+  print("\n")
+  key = 0
+  for character in possChar:
+    for character in initMsg:
+      if character in possChar:
+        initPos = possChar.find(character)
+        shiftPos = initPos - int(key)
+        wrap()
+        shiftMsg += possChar[shiftPos]
+      else:
+        shiftMsg+= character
+    print(shiftMsg + " | Key: "+ str(key))
+    key = key + 1
+
+    # Print the shifted message
+    shiftMsg = ""
+
+# I also did this whole program without using any of the videos :P
